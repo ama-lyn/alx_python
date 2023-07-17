c48ad7dd839146ac141e0a0dd042f2338e4662e3
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-17 23:11:19 +0300

    Correction

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "School"
-word_first_3 = "Sch"
-word_last_2 = "ol"
-middle_word = "choo"
+word = "Holberton"
+word_first_3 = "Hol"
+word_last_2 = "on"
+middle_word = "olberto"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
