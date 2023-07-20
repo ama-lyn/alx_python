677687189047b18f603400faca2aa3f2b629220b
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-20 17:37:24 +0300

    Temperature to Fahrenheit

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
-
-
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
