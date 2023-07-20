61159a573b047e7e77026df8c079c7e4c5f72537
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-20 17:34:35 +0300

    Prime number

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -9,10 +9,3 @@ def is_prime(number):
             if number % i == 0:
                 return False
         return True
-
-
-print(is_prime(2))   # Output: False (2 is not prime)
-print(is_prime(3))   # Output: True (3 is prime)
-print(is_prime(11))  # Output: True (11 is prime)
-print(is_prime(20))  # Output: False (20 is not prime)
-print(is_prime(23))  # Output: True (23 is prime)
