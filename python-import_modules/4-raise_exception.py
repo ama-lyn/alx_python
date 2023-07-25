4894f292e5c7f227caf2ea7a09a02babaf3dbd73
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-25 07:30:39 +0300

    Raise exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,6 @@
-def raise_exception():
+def raise_and_handle_type_exception():
     """Raise and handle a TypeError."""
     try:
         x = "string" + 42  # This will cause a TypeError to be raised
-
-    except TypeError:
-        print("Exception raised")
+    except TypeError as e:
+        print("Exception raised.")
