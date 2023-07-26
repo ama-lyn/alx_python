c7e13a88282265d80ac8c0a533a7a12135b044af
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-26 09:01:51 +0300

    Update

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Loop through each row of the matrix
     for row in matrix:
-        # Create an empty string to store the formatted row
-        row_str = ""
-        # Loop through each element of the row
-        for elem in row:
-            # Use str.format() to add the element to the string with a fixed width of 3
-            row_str += "{:3}".format(elem)
-        # Print the row string
-        print(row_str)
+        for num in row:
+            print("{:3}".format(num), end=" ")
+        print()
