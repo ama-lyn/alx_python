93b1c3a6405cc1a0061abc367ccd362e6c1311ea
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-27 19:35:35 +0300

    Common elements in a set

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -5,11 +5,3 @@ def common_elements(set_1, set_2):
         if item in set_2:
             common_set.add(item)
     return common_set
-
-
-# Output: ['C']
-
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
