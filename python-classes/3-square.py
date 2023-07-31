6ac244f708f2896c10d344a0a41ec113e94fa5c6
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-31 14:14:43 +0300

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -27,11 +27,12 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return (self.__size ** 2)
