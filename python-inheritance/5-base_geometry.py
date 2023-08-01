454092ed90b7bc1c816323ec0151a4b909c7aa4f
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-01 21:00:28 +0300

    Integer Validator

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,20 @@
+'''
+This module defines a class that raises an Exception with the 
+message area() is not implemented
+'''
+
+
+class BaseGeometry:
+    '''
+    Simple class that raises an exception
+    '''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("<{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
