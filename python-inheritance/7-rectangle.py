0ac978b12b9fa7739b6443ae614a855abd2a2735
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-01 23:03:04 +0300

    Area

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,35 @@
+'''
+This module defines a class that raises an Exception with the 
+message area() is not implemented
+'''
+
+
+class BaseGeometry:
+    '''
+    Simple class that raises an exception
+    '''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width*self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
