fa20d9e608855d5dd9077a61f464a13ec5b1a6e9
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-03 15:04:07 +0300

    Update

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -53,4 +53,5 @@ class Square(Rectangle):
         super().__init__(size, size)
 
     def __str__(self):
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return "[Square] {}/{}".format(self._Rectangle__width,
+                                       self._Rectangle__height)
