f86dedb26371ade7713c5bae9c67dc5c14e9aabb
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-03 14:57:21 +0300

    Inheritance

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,53 @@
+'''
+This module defines a class that raises an Exception with the 
+message area() is not implemented
+'''
+
+
+class BaseGeometry:
+    '''
+    Simple class that raises an exception
+    '''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    '''
+    This is a simple class to represent a rectangle.
+
+   Attributes:
+       width: private attribute.
+       height: private attribute.
+       Inherited function-integer_validator
+    '''
+
+    def __init__(self, width, height):
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width*self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
