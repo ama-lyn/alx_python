5827be1deeaa5f8be6c9b8abc0034447b819d585
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-05 23:33:06 +0300

    BaseGeometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,22 +3,19 @@ This module is an empty class.
 '''
 
 
-class BaseGeometry:
-    '''
-    Simple empty class 
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+   Simple empty class 
     Contains a function tha excludes the attribute 
     __init_subclass_ from dir()
-    '''
-
-    def __dir__(cls) -> None:
-        # get list of all attributes for this class and exclude __init_subclass
-        attributes = super().__dir__()
-
-        list_to_return = []
-
-        for attr in attributes:
-            if attr != "__init_subclass__":
-                # append this to the list_to_return
-                list_to_return.append(attr)
-
-        return list_to_return
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
