7c189a51b2ae61253109fb0db09f3e9ad66f5d3d
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-05 23:36:24 +0300

    Inheritance

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -4,10 +4,22 @@ message area() is not implemented
 '''
 
 
-class BaseGeometry:
-    '''
-    Simple class that raises an exception
-    '''
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+   Simple empty class 
+    Contains a function tha excludes the attribute 
+    __init_subclass_ from dir()
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
     def area(self):
         raise Exception("area() is not implemented")
