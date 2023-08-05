00bdb671e5ef3f0cab80068124bbcfd5e7ab188f
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-05 23:37:23 +0300

    Inheritance

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -3,10 +3,22 @@ This module defines classes for geometry calculations.
 '''
 
 
-class BaseGeometry:
-    '''
-    A base class for geometry calculations.
-    '''
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+   Simple empty class 
+    Contains a function tha excludes the attribute 
+    __init_subclass_ from dir()
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
     def area(self):
         raise Exception("area() is not implemented")
