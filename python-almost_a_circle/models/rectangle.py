63031ba6c8b4a0824256070c81ec15600d3d2da1
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-07 19:30:18 +0300

    Validation of the attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,10 +22,10 @@ class Rectangle(Base):
             id (int): The unique ID of the rectangle.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
