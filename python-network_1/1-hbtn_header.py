9bdff3092ca1ecb166e2c9c147194a6ce8b42197
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-09 15:16:55 +0300

    Status

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,9 +6,6 @@ in the response header
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python script_name.py <URL>")
-    sys.exit(1)
 
 url = sys.argv[1]
 response = requests.get(url)
