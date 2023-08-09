f280e66030d8d36edd241e304c684084d67fb307
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-09 15:18:33 +0300

    Update

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 '''
 A module that contains a Python script that takes in a URL, sends a request 
 to the URL and displays the value of the variable X-Request-Id 
-in the response header
+in the response header.
 '''
 import requests
 import sys
