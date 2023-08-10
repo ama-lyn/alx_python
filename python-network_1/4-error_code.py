218365a3be5470a308bcc8c2929f36d7c6d3579f
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-10 08:38:19 +0300

    Error codes

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,14 +1,12 @@
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python script_name.py <URL>")
-    sys.exit(1)
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-url = sys.argv[1]
-response = requests.get(url)
+    response = requests.get(url)
 
-if response.status_code >= 400:
-    print("Error codde:", response.status_code)
-else:
-    print(response.text)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
