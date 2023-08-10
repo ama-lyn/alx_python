60752792342ef47e187670f88ddf77f060be2a3e
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-10 08:47:26 +0300

    Error codes

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,22 +1,22 @@
-import requests
-import sys
-
 '''
 Module that contains a a Python script that takes in a URL,
 sends a request to the URL and displays the body of the 
 response.
 '''
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
-        sys.exit(1)
+import requests
+import sys
 
-    url = sys.argv[1]
 
-    response = requests.get(url)
+if len(sys.argv) != 2:
+    print("Usage: python script_name.py <URL>")
+    sys.exit(1)
 
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
-        print(response.text)
+url = sys.argv[1]
+
+response = requests.get(url)
+
+if response.status_code >= 400:
+    print("Error code:", response.status_code)
+else:
+    print(response.text)
