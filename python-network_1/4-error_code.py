36fe22001fd4be2e3e68213b7ecf1e21ad137754
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-10 08:43:47 +0300

    Error codes

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,6 +1,12 @@
 import requests
 import sys
 
+'''
+Module that contains a a Python script that takes in a URL,
+sends a request to the URL and displays the body of the 
+response.
+'''
+
 if __name__ == "__main__":
     url = sys.argv[1]
 
