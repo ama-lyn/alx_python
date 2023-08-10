278bfb9f5f558a309acb1df58957a32f91f93836
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-10 08:45:45 +0300

    Error codes

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -8,6 +8,10 @@ response.
 '''
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+        sys.exit(1)
+
     url = sys.argv[1]
 
     response = requests.get(url)
