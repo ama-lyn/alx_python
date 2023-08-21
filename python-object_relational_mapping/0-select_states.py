997e4f08a716ab72d9d73aaefc0d36071f4535aa
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-21 15:01:53 +0300

    Implement MySQLdb

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server
+    DBconnection = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, database=database_name)
+
+    # Create a cursor object to interact with the query
+    cursor = DBconnection.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT id,names FROM states ORDER BY id ASC")
+
+    # Fetch all the data according to the query
+    states = cursor.fetchall()
+
+    # Display
+    for state in states:
+        print(state)
+
+    # Close the cursor and the connection (Clean Up)
+    cursor.close()
+    DBconnection.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,9 @@
+# Python Object Relational Mapping (ORMs)
+
+The purpose of an ORM is to abstract the storage to the usage.
+
+This allows for a more natural way to interact with data and also provides some level of security by hiding details about how the database works from you application
+
+In this repository I will be linking two amazing worlds: Databases and Python!
+
+Using the module MySQLdb to connect to a MySQL database and execute your SQL queries and the module SQLAlchemy an Object Relational Mapper (ORM).
