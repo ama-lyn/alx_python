cb68e2e4cf45199a35fd689fcc150f77a265d1f1
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-23 12:05:44 +0300

    Safe from SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''
+Write a script that takes in an argument and
+displays all values in the states table of 
+hbtn_0e_0_usa where name matches the argument.
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Establishing connection to MySQL server
+    DBconnection = MySQLdb.connect(host='localhost', port=3306,
+                                   user=username,
+                                   passwd=password,
+                                   database=database)
+
+    cursor = DBconnection.cursor()
+
+    sql_query = "SELECT id, name\
+             FROM states\
+             WHERE BINARY name =%s\
+             ORDER BY id ASC"
+    state_name = sys.argv[4]
+
+    # Execute the query with the provided state name
+    cursor.execute(sql_query, (state_name, ))
+
+    # Fetch all the matching rows
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and database connection
+    cursor.close()
+    DBconnection.close()
