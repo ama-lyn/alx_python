0a3a30804050161f46c61896e1353f2df4fb5a59
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-23 15:06:42 +0300

    Safe from SQL Injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 Write a script that takes in an argument and
-displays all values in the states table of 
+displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument.
 '''
 
