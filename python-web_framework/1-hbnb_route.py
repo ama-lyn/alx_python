b50b9aba0a02f492bf422e78478d6b127645890a
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-29 10:13:21 +0300

    Module documentation

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """
-This module starts a web aplication via Flask
+This module starts a web aplication using Flask
+It defines routes and handlers for different URLs to demonstrate
+the basic usage of the Flask web framework.
 """
 
 from flask import Flask
@@ -11,13 +13,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    A route handler for the root URL '/'. It returns a greeting message.
+
+    Returns:
+        str: Greeting message "Hello HBNB!".
+    """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
+    """
+    A route handler for the URL '/hbnb'. It returns a message "HBNB".
+
+    Returns:
+        str: Message "HBNB".
+    """
     return "HBNB"
 
 
 if __name__ == '__main__':
+    """
+    Main entry point of the script. Starts the Flask app.
+    """
     app.run(host='0.0.0.0', port=5000)
